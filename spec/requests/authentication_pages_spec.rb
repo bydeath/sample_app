06e93383542868185f7545253c97877695eba856
bryant <bydeath@sina.com>
2014-09-12 22:58:51 +0800

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -99,18 +99,27 @@ describe "Authentication" do
                   it { should have_title('Sign in') }
               end
 
+              describe "visiting the following page" do
+                before { visit following_user_path(user) }
+                it { should have_title('Sign in') }
+              end
+
+              describe "visiting the followers page" do
+                before { visit followers_user_path(user) }
+                it { should have_title('Sign in') }
+              end
           end
 
           describe "as non-admin user" do
-              let(:user) { FactoryGirl.create(:user) }
-              let(:non_admin) { FactoryGirl.create(:user) }
+            let(:user) { FactoryGirl.create(:user) }
+            let(:non_admin) { FactoryGirl.create(:user) }
 
-              before { sign_in non_admin, no_capybara: true }
+            before { sign_in non_admin, no_capybara: true }
 
-              describe "submitting a DELETE request to the Users#destroy action" do
-                  before { delete user_path(user) }
-                  specify { expect(response).to redirect_to(root_path) }
-              end
+            describe "submitting a DELETE request to the Users#destroy action" do
+              before { delete user_path(user) }
+              specify { expect(response).to redirect_to(root_path) }
+            end
           end
 
           describe "in the Microposts controller" do
@@ -122,6 +131,18 @@ describe "Authentication" do
 
             describe "submitting to the destroy action" do
               before { delete micropost_path(FactoryGirl.create(:micropost)) }
+              specify { expect(response).to redirect_to(signin_path) }
+            end
+          end
+
+          describe "in the Relationships controller" do
+            describe "submitting to the create action" do
+              before { post relationships_path }
+              specify { expect(response).to redirect_to(signin_path) }
+            end
+
+            describe "submitting to the destroy action" do
+              before { delete relationship_path(1) }
               specify { expect(response).to redirect_to(signin_path) }
             end
           end
